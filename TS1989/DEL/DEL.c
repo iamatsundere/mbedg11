540bef3f82cb878a07f10b40a9780574b3cb19cb
phuctt <phuc@vng>
2016-12-16 13:12:41 +0700

    LED commit

--- TS1989/DEL/DEL.c
+++ TS1989/DEL/DEL.c
@@ -8,23 +8,26 @@
 #define CTRL_PORT 	PORTC //PORTC is connected to control lines of DotMatrix
 #define CTRL_DDR	DDRC
 
-void DOTputChar75(uint8_t chr){  
+void DOTputChar75(uint8_t chr) {
 	uint8_t line=1,i;
 	uint16_t tchr=chr;
-	for(i=0;i<5;i++) {
+	for(i=0; i<5; i++) {
 		CTRL_PORT=line;
-		DATA_PORT=~pgm_read_byte(&font7x5[((tchr - 32) * 5) + i]);			
+		DATA_PORT=~pgm_read_byte(&font7x5[((tchr - 32) * 5) + i]);
 		line<<=1;
-		_delay_loop_2(1000);
+		_delay_ms(10);
 	}
+	_delay_ms(200);
 }
 
-int main(void)
-{
+int main(void) {
 	DATA_DDR=0xFF; // DATA_PORT is selected as an ouput PORT
 	CTRL_DDR=0xFF;
-	while(1){
-		DOTputChar75('4');
+	while(1) {
+		char s[10]= {'0','1','2','3','4','5','6','7','8','9'};
+		for(int i=0; i<10; i++) {
+			DOTputChar75(s[i]);
+		}
 	}
 	return 0;
 }
--- TS1989/DEL/dotmatrix.c
+++ TS1989/DEL/dotmatrix.c
@@ -8,22 +8,21 @@
 #define CTRL_PORT 	PORTC //PORTC is connected to control lines of DotMatrix
 #define CTRL_DDR	DDRC
 
-void DOTputChar75(uint8_t chr){  
+void DOTputChar75(uint8_t chr) {
 	uint8_t line=1,i;
 	uint16_t tchr=chr;
-	for(i=0;i<5;i++) {
+	for(i=0; i<5; i++) {
 		CTRL_PORT=line;
-		DATA_PORT=~pgm_read_byte(&font7x5[((tchr - 32) * 5) + i]);			
+		DATA_PORT=~pgm_read_byte(&font7x5[((tchr - 32) * 5) + i]);
 		line<<=1;
 		_delay_loop_2(1000);
 	}
 }
 
-int main(void)
-{
+int main(void) {
 	DATA_DDR=0xFF; // DATA_PORT is selected as an ouput PORT
 	CTRL_DDR=0xFF;
-	while(1){
+	while(1) {
 		DOTputChar75('4');
 	}
 	return 0;
