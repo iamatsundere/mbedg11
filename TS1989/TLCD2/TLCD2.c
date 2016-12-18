db9a97c66964475070c633fdbd32972fa81cdfb4
phuctt <phuc@vng>
2016-12-18 08:55:45 +0700

    print 5

--- TS1989/TLCD2/TLCD2.c
+++ TS1989/TLCD2/TLCD2.c
@@ -8,51 +8,27 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-void QuetTungCot()
+void Drawing()
 {
 	/*quét t?ng c?t*/
 	PORTC = 0b0000001;//ch?n c?t ??u tiên
-	PORTD = 0b0000000;//ch?n hàng 1
+	PORTD = 0b0110000;//ch?n hàng 1
 	_delay_ms(10);
 	PORTC = 0b0000010;//ch?n c?t th? hai
-	PORTD = 0b1110110;//ch?n hàng 1
+	PORTD = 0b0110110;//ch?n hàng 1
 	_delay_ms(10);
 	PORTC = 0b0000100;//ch?n c?t th? ba
-	PORTD = 0b1110110;//ch?n hàng tat ca cac hang
+	PORTD = 0b0110110;//ch?n hàng tat ca cac hang
 	_delay_ms(10);
 	PORTC = 0b0001000;//ch?n c?t th? 4
-	PORTD = 0b1110110;//ch?n hàng 1
+	PORTD = 0b0110110;//ch?n hàng 1
 	_delay_ms(10);
 	PORTC = 0b0010000;//ch?n c?t th? 5
-	PORTD = 0b1110000;//ch?n hàng 1
+	PORTD = 0b0000110;//ch?n hàng 1
 	_delay_ms(10);
 }
 
-void QuetTungHang()
-{
-	/*quet tung hang*/
-	PORTD = 0b1111011;//ch?n hàng 1
-	PORTC = 0b0011111;//chon 5 cot
-	_delay_ms(5);
-	PORTD = 0b1111101;//ch?n hàng 2
-	PORTC = 0b0000100;//chon cot thu 3
-	_delay_ms(5);
-	PORTD = 0b1111011;//ch?n hàng 3
-	PORTC = 0b0000100;//chon cot thu 3
-	_delay_ms(5);
-	PORTD = 0b1110111;//ch?n hàng 4
-	PORTC = 0b0000100;//chon cot thu 3
-	_delay_ms(5);
-	PORTD = 0b1101111;//ch?n hàng 5
-	PORTC = 0b0000100;//chon cot thu 3
-	_delay_ms(5);
-	PORTD = 0b1011111;//ch?n hàng 6
-	PORTC = 0b0000100;//chon cot thu 3
-	_delay_ms(5);
-	PORTD = 0b0111111;//ch?n hàng 7
-	PORTC = 0b0000100;//chon cot thu 3
-	_delay_ms(5);
-}
+
 
 int main(void)
 {
@@ -63,7 +39,7 @@ int main(void)
 	DDRD = 0b01111111;
 	while (1)
 	{
-		QuetTungHang();
+		Drawing();
 		//QuetTungCot();
 	}
 }
