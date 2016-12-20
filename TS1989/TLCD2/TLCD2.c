da8ec3eefcaeb0d8df15d57bf818c94a8997f418
phuctt <phuc@vng>
2016-12-20 18:37:35 +0700

    update

--- TS1989/TLCD2/TLCD2.c
+++ TS1989/TLCD2/TLCD2.c
@@ -3,44 +3,36 @@
  *
  * Created: 12/18/16 8:08:05 AM
  *  Author: truon
- */ 
+ */
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-void Drawing()
-{
-	/*quét t?ng c?t*/
-	PORTC = 0b0000001;//ch?n c?t ??u tiên
-	PORTD = 0b0110000;//ch?n hàng 1
-	_delay_ms(10);
-	PORTC = 0b0000010;//ch?n c?t th? hai
-	PORTD = 0b0110110;//ch?n hàng 1
-	_delay_ms(10);
-	PORTC = 0b0000100;//ch?n c?t th? ba
-	PORTD = 0b0110110;//ch?n hàng tat ca cac hang
-	_delay_ms(10);
-	PORTC = 0b0001000;//ch?n c?t th? 4
-	PORTD = 0b0110110;//ch?n hàng 1
-	_delay_ms(10);
-	PORTC = 0b0010000;//ch?n c?t th? 5
-	PORTD = 0b0000110;//ch?n hàng 1
-	_delay_ms(10);
+void Drawing() {
+	PORTA = 0b0000001;
+	PORTB = 0b0110000;
+	_delay_ms(100);
+	PORTA = 0b0000010;
+	PORTB = 0b0110110;
+	_delay_ms(100);
+	PORTA = 0b0000100;
+	PORTB = 0b0110110;
+	_delay_ms(100);
+	PORTA = 0b0001000;
+	PORTB = 0b0110110;
+	_delay_ms(100);
+	PORTA = 0b0010000;
+	PORTB = 0b0000110;
+	_delay_ms(100);
 }
 
 
 
-int main(void)
-{
-	/* Replace with your application code */
-	/*Khai báo các chân nào là chân truy?n tín hi?u ra các chân ra s? ?? là 1
-	th? t? t??ng ?ng các chân là PORT7-POR6-PORT5...PORT0*/
-	DDRC = 0b00011111;
-	DDRD = 0b01111111;
-	while (1)
-	{
+int main(void) {
+	DDRA = 0b0011111;
+	DDRB = 0b1111111;
+	while (1) {
 		Drawing();
-		//QuetTungCot();
 	}
 }
 
